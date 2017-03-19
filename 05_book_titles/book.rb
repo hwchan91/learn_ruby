cd5b6830ee2b252999ebcc167bbd52c7ffe14c16
Dion Chan <hwchan91@outlook.com>
2017-03-19 22:08:29 +0800

    amended 05, finished 06; finished all

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -7,21 +7,11 @@ class Book
 
   def title=(name)
     words = name.split
-#    ind_words.map! do |word|
-#      word = word.capitalize
-#    end
-#    @title = ind_words * " "
-#  end
-
-
-    words[0] = words[0].capitalize
-    words[1,words.length-1].map! do |word|
-      if not (word == "in" || word == "and"|| word == "of"|| word == "at"|| \
-         word == "on"|| word == "over"|| word == "the" || word == "a" || word == "an")
-        word.replace (word.capitalize)
-      end
-    end
-    @title = words * (" ")
+    words_omit_first = words[1,words.length-1]
+#    words_omit_first.map! { |word| (not (word == "in" || word == "and"|| word == "of"|| word == "at"|| \
+#         word == "on"|| word == "over"|| word == "the" || word == "a" || word == "an")) ? word.capitalize : word }
+    words_omit_first.map! { |word| (["in","and","of","over","at","on","the","a","an"].include?(word)) ? word : word.capitalize }
+    @title = (words[0].capitalize + " " + words_omit_first * (" ")).strip
   end
 
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,44 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    hour = 0
+    minute = 0
+    second = 0
+    if (@seconds > 3600)
+      hour = @seconds / (3600)
+      minute = (@seconds - hour * 3600 ) /60
+      second = @seconds - hour * 3600 - minute * 60
+    elsif (@seconds > 60)
+      minute = @seconds / 60
+      second = @seconds - minute * 60
+    else
+      second = @seconds
+    end
+
+    if hour < 10
+      hour_string = "0#{hour}"
+    else
+      hour_string = "#{hour}"
+    end
+
+    if minute < 10
+      minute_string = "0#{minute}"
+    else
+      minute_string = "#{mintue}"
+    end
+
+    if second < 10
+      second_string = "0#{second}"
+    else
+      second_string = "#{second}"
+    end
+
+    "#{hour_string}:#{minute_string}:#{second_string}"
+  end
+
 end
